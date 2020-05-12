1562a1611c3c556715c02fd33d45c962ee139ae3
Rishikesh Kadam <rishikesh1419@gmail.com>
2020-05-12 22:13:43 +0530

    First commit

---
+++ src/main/java/com/rishikesh/covid19casestracker/Covid19CasesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rishikesh.covid19casestracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19CasesTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19CasesTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rishikesh/covid19casestracker/Covid19CasesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishikesh.covid19casestracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19CasesTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
